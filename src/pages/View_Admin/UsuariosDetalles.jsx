7e09c394853de74faf2ffd79b8a08cb6798a563c
hilñasduykasduyuyasd <jkasdasdghkghkkasdghasdghksd>
2025-04-21 16:44:44 -0500

    Actualizacion de la parte admin

--- src/pages/View_Admin/UsuariosDetalles.jsx
+++ src/pages/View_Admin/UsuariosDetalles.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar_Admin from "../../components/Navbar_Admin";
+
 import Footer from "../../components/Footer";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import {
